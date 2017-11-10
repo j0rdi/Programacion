34c20fedb758dd71cffd288061824efc21b604a2
Jordi Vidal <32283705+j0rdi@users.noreply.github.com>
2017-11-10 17:51:26 +0100

    Add files via upload

--- Practica 7/Practica 7 Ejercicio 1.py
+++ Practica 7/Practica 7 Ejercicio 1.py
@@ -1,11 +1,13 @@
 """Jordi Vidal Palou. Pràctica 7. Ejercicio 1
 Escribe un programa que pida un texto por pantalla, este texto lo pase como parámetro a un
-procedimiento, y éste lo imprima primero todo en minúsculas y luego todo en mayúsculas. """
+procedimiento, y éste lo imprima primero todo en minúsculas y luego todo en mayúsculas. 
+"""
 
 
+def letras(hola):
+    print a.upper()
+    print a.lower() 
 
-print "Escribe un texto y te lo pasaré a mayúsculas y minúsculas"
-a=raw_input()
-b=a.swapcase()
-print a
-print b
+a=raw_input("Escribe un texto y te lo pasaré a mayúsculas y minúsculas: ")
+
+letras(a)
