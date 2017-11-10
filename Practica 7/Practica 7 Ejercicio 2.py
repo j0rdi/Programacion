8ad39737b0e0414a2199542bca46c7e8cf58151c
Jordi Vidal <32283705+j0rdi@users.noreply.github.com>
2017-11-10 17:51:49 +0100

    Add files via upload

--- Practica 7/Practica 7 Ejercicio 2.py
+++ Practica 7/Practica 7 Ejercicio 2.py
@@ -1,8 +1,12 @@
 """Jordi Vidal Palou. Pràctica 7. Ejercicio 2
 Escribe un programa que lea el nombre y los dos apellidos de una persona (en tres cadenas de
 caracteres diferentes), los pase como parámetros a una función, y ésta debe unirlos y devolver
-una única cadena. La cadena final la imprimirá el programa por pantalla."""
+una única cadena. La cadena final la imprimirá el programa por pantalla.
+"""
 
+def unir(a,b,c):
+    return a+" "+b+" "+c
+    
 
 
 print "Dime tu nombre"
@@ -12,5 +16,6 @@ b=raw_input()
 print "Dime tu segundo apellido"
 c=raw_input()
 
-print "Tu nomber completo es",a ,b ,c
+resultado=unir(a,b,c)
+print resultado
 
